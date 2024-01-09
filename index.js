ec26ba8399c6ff9b34d0485de93d117a36e8eb4a
Tamojit <69034710+crysre@users.noreply.github.com>
2024-01-09 06:38:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var randomNumber1 = Math.random()*6
+randomNumber1 = Math.floor(randomNumber1)
+console.log(randomNumber1)
+
+var randomNumber2 = Math.random()*6
+randomNumber2 = Math.floor(randomNumber2)
+console.log(randomNumber2)
+
+if(randomNumber1 == 0){
+    document.querySelector(".img1").setAttribute("src", "./images/dice1.png")
+}else if(randomNumber1 == 1){
+    document.querySelector(".img1").setAttribute("src", "./images/dice2.png")
+
+}else if(randomNumber1 == 2){
+    document.querySelector(".img1").setAttribute("src", "./images/dice3.png")
+
+}else if(randomNumber1 == 3){
+    document.querySelector(".img1").setAttribute("src", "./images/dice4.png")
+
+}else if(randomNumber1 == 4){
+    document.querySelector(".img1").setAttribute("src", "./images/dice5.png")
+
+}
+
+if(randomNumber2 == 0){
+    document.querySelector(".img2").setAttribute("src", "./images/dice1.png")
+}else if(randomNumber2 == 1){
+    document.querySelector(".img2").setAttribute("src", "./images/dice2.png")
+
+}else if(randomNumber2 == 2){
+    document.querySelector(".img2").setAttribute("src", "./images/dice3.png")
+
+}else if(randomNumber2 == 3){
+    document.querySelector(".img2").setAttribute("src", "./images/dice4.png")
+
+}else if(randomNumber2 == 4){
+    document.querySelector(".img2").setAttribute("src", "./images/dice5.png")
+
+}
+
+if(randomNumber1 == randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!"
+}else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩"
+}
